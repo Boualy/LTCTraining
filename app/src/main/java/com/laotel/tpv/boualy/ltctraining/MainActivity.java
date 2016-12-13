48fb0d2ac186642d3b4c9248dc27de40f2b6af14
Boualy <boualytpv@gmail.com>
2016-12-13 14:35:23 +0700

    Intent to SignUp finished

--- app/src/main/java/com/laotel/tpv/boualy/ltctraining/MainActivity.java
+++ app/src/main/java/com/laotel/tpv/boualy/ltctraining/MainActivity.java
@@ -1,15 +1,32 @@
 package com.laotel.tpv.boualy.ltctraining;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initInstances();
+    }
 
+    private void initInstances() {
+        btn_sign_up = (Button) findViewById(R.id.btn_sign_up);
+        btn_sign_up.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btn_sign_up){
+            startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+        }
 
     }
 }
