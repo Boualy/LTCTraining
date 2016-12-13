f3e3d262b6786c054e070eaed1cbe92197e95e12
Boualy <boualytpv@gmail.com>
2016-12-13 16:39:03 +0700

    FindViewByID Done

--- app/src/main/java/com/laotel/tpv/boualy/ltctraining/SignUpActivity.java
+++ app/src/main/java/com/laotel/tpv/boualy/ltctraining/SignUpActivity.java
@@ -2,12 +2,30 @@ package com.laotel.tpv.boualy.ltctraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+    // Explicit
+    private EditText nameEditTex, userEditText, passEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        nameEditTex = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passEditText = (EditText) findViewById(R.id.editText3);
+
+    } // Main Method
+
+    public void clickSignUpSign(View view) {
+
     }
-}
+
+
+} // Main Class
