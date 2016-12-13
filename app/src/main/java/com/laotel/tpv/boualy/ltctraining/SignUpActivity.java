c97ea312fb05061b560d4e33381bb9e2251930b9
Boualy <boualytpv@gmail.com>
2016-12-13 16:56:37 +0700

    Finished Date: 13-Dec-2016

--- app/src/main/java/com/laotel/tpv/boualy/ltctraining/SignUpActivity.java
+++ app/src/main/java/com/laotel/tpv/boualy/ltctraining/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.laotel.tpv.boualy.ltctraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -10,6 +11,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     // Explicit
     private EditText nameEditTex, userEditText, passEditText;
+    private String nameString, userString, passString;
 
 
     @Override
@@ -25,7 +27,18 @@ public class SignUpActivity extends AppCompatActivity {
 
     public void clickSignUpSign(View view) {
 
-    }
+        nameString = nameEditTex.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passString = passEditText.getText().toString().trim();
+
+        // Check Space
+
+        if (nameString.equals("") || userString.equals("") || passString.equals("")) {
+            // have space
+            Log.d("13decV1", "Have space");
+        }
+
+    } // ClickSign
 
 
 } // Main Class
